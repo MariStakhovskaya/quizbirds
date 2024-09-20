a097dd0206edcef62863562a30e57d60c1cf1784
Maria Stachovski <maryia.jegorova@gmail.com>
2024-09-20 13:16:54 +0200

    fix: fix result bug

--- pages/results/results.js
+++ pages/results/results.js
@@ -6,8 +6,10 @@ let myScore = localStorage.getItem('gameResults');
 
 if (myScore == 30) {
     resultsBlock.innerHTML = `Поздравляем с победой!!! Вы прошли викторину и набрали ${myScore} из 30`
-} else {
+} else if (myScore > 0){
     resultsBlock.innerHTML = `Поздравляем! Вы прошли викторину и набрали ${myScore} из 30`
+} else {
+    resultsBlock.innerHTML = `Пройдите викторину, нажав на вкладку Quiz`
 }
 
 
